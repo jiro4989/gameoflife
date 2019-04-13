200f1548293b1542c736deefba02f3d973be92bc
jiro4989 <jiroron666@gmail.com>
2019-04-13 19:42:09 +0900

    :fire: Delete sample code

--- src/lifegame.nim
+++ src/lifegame.nim
@@ -78,20 +78,3 @@ proc print*(board: Board) =
               else: "D"
       s.add c & "|"
     echo s
-
-when isMainModule:
-  when false:
-    from os import sleep
-
-    var board: Board = @[
-      @[dead, dead, dead, dead, dead],
-      @[dead, dead, dead, dead, dead],
-      @[dead, live, live, live, dead],
-      @[dead, dead, dead, dead, dead],
-      @[dead, dead, dead, dead, dead],
-    ]
-    while true:
-      board.nextStep()
-      board.print
-      echo "-----------------------------------"
-      sleep(1000)
